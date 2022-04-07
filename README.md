1374bb8b4f9a2366300e89ba6e265d0eefffb828
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2022-04-07 17:28:33 +0100

    webpack environment established

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project_manager
+
 An application in which users can manage projects and tasks; this is an assignment from The Odin Project.
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
