271aa05120b95d0b3b61a529547daf441a8c2f84
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2022-04-09 11:02:09 +0100

    restructure task-list style to accomodate multi-project styles on same elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/style.css'
 
-import { sidebar } from './sidebar'
+import { sidebar } from './sidebar';
+import { pubsub } from './pubsub';
 
 sidebar.render()
 
@@ -11,3 +12,43 @@ document.querySelectorAll('.checkbox').forEach(checkbox => {
     parent.classList.toggle('complete')
   })
 })
+
+const newTaskForm = (() => {
+  const newTaskFormContainer = document.createElement('li');
+
+  newTaskFormContainer.classList.add('task-form-container');
+  newTaskFormContainer.innerHTML = `
+    <form action="#" method="post" class="new-task-form">
+      <p class="description-container">
+        <label for="description">Task:</label>
+        <input type="text" name="description" id="description" class="new-task-input">
+      </p>
+      <p class="deadline-container">
+        <label for="deadline">Deadline:</label>
+        <input type="time" name="deadline" id="deadline" class="new-task-input">
+      </p>
+      <button type="button" id="add-task" class="form-btn add">Add</button>
+      <button type="button" id="cancel" class="form-btn cancel">Cancel</button>
+    </form>
+  `
+
+  const render = (parent, target) => parent.insertBefore(newTaskFormContainer, target);
+
+  return {
+    render
+  }
+})();
+
+
+
+function showNewTaskForm(ev) {
+  const prevElement = ev.target.previousElementSibling;
+
+  if (prevElement.classList.contains('task-form-container')) return
+
+  newTaskForm.render(prevElement.parentElement, ev.target);
+}
+
+const newTaskBtn = document.getElementById('new-task-btn');
+
+newTaskBtn.addEventListener('click', showNewTaskForm);
---
+++ src/pubsub.js
@@ -0,0 +1,19 @@
+export const pubsub = (() => {
+  const events = {}
+
+  const subscribe = (evName, fn) => {
+    events[evName] = events[evName] || [];
+    events[evName].push(fn);
+  }
+
+  const publish = (evName, data) => {
+    if (events[evName]) {
+      events[evName].forEach(fn => fn(data));
+    }
+  }
+
+  return {
+    subscribe,
+    publish
+  }
+})();
