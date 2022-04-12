14e6b2208f43e05c8e36c2c70a052fc0c0eb64c6
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2022-04-12 13:37:50 +0100

    convert Project and Task constructors into factory functions

--- src/projectsModule.js
+++ src/projectsModule.js
@@ -1,44 +1,44 @@
 import { pubsub } from "./pubsub";
 
-function Project(name, type = 'custom', deadline = null) {
-  this.name = name;
-  this.deadline = deadline;
-  this.type = type;
-  this.tasks = [];
-
-  this.addTask = function (description, deadline) {
-    this.tasks.push(new Task(description, deadline))
-  }
-
-  this.saveToLocalStorage = function () {
-    const saveData = {
-      name: this.name,
-      deadline: this.deadline,
-      type: this.type,
-      tasks: this.tasks
-    }
-
-    localStorage.setItem(this.name, JSON.stringify(saveData));
-  }
-}
-
-function Task(description, deadline, isComplete = false) {
-  this.description = description;
-  this.deadline = deadline;
-  this.isComplete = isComplete;
-}
-
-const monday = new Project('Monday', 'default');
-const tuesday = new Project('Tuesday', 'default');
-const wednesday = new Project('Wednesday', 'default');
-const thursday = new Project('Thursday', 'default');
-const friday = new Project('Friday', 'default');
-const saturday = new Project('Saturday', 'default');
-const sunday = new Project('Sunday', 'default');
-
 export const projectsModule = (() => {
+  const Project = (name, type = 'custom', deadline = null) => {
+    const Task = (description, deadline, isComplete = false) => {
+      return {
+        description,
+        deadline,
+        isComplete
+      }
+    }
+  
+    return {
+      name,
+      type,
+      tasks: [],
+      deadline,
+      addTask(description, deadline) {
+        this.tasks.push(Task(description, deadline))
+      },
+      saveToLocalStorage() {
+        localStorage.setItem(this.name, JSON.stringify({
+          name: this.name,
+          deadline: this.deadline,
+          type: this.type,
+          tasks: this.tasks
+        }));
+      }
+    }
+  }
+
   const projects = [];
-  const weekdays = [monday, tuesday, wednesday, thursday, friday, saturday, sunday];
+  const weekdays = [
+    Project('Monday', 'default'),
+    Project('Tuesday', 'default'),
+    Project('Wednesday', 'default'),
+    Project('Thursday', 'default'),
+    Project('Friday', 'default'),
+    Project('Saturday', 'default'),
+    Project('Sunday', 'default')
+  ];
 
   const getProject = (id) => [...projects, ...weekdays].filter(project => project.name == id)[0];
 
@@ -63,28 +63,22 @@ export const projectsModule = (() => {
 
   const createNewProject = (formDataObj) => {
     const formatDeadline = formDataObj.deadline.split('-').reverse().join('-');
-    const newProject = new Project(formDataObj.name, 'custom', formatDeadline == '' ? null : formatDeadline);
+    const newProject = Project(formDataObj.name, 'custom', formatDeadline == '' ? null : formatDeadline);
 
     projects.push(newProject)
-
     newProject.saveToLocalStorage();
-
     pubsub.publish('singleProjectRetrieved', newProject)
   }
   
   const handleTabChange = (tabName) => {
-    if (tabName == 'projects') {
-      sendProjectSummary('All Projects', projects);
-    } else if (tabName == 'week') {
-      sendProjectSummary('Week', weekdays);
-    } else {
-      sendDefaultProject();
-    }
+    if (tabName == 'projects') sendProjectSummary('All Projects', projects);
+    if (tabName == 'week') sendProjectSummary('Week', weekdays);
+    if (tabName == 'today') sendDefaultProject();
   }
 
   const loadCustomProjects = (projectList) => {
     projectList.forEach(project => {
-      const newProject = new Project(project.name, project.deadline)
+      const newProject = Project(project.name, project.deadline)
 
       project.tasks.forEach(task => {
         newProject.addTask(task.description, task.deadline, task.isComplete)
@@ -107,7 +101,7 @@ export const projectsModule = (() => {
   const getAllLocalStorageKeys = () => {
     const keys = [];
 
-    for (let i = 0; i < window.localStorage.length; i ++) {
+    for (let i = 0; i < window.localStorage.length; i++) {
       keys.push(localStorage.key(i))
     }
 
